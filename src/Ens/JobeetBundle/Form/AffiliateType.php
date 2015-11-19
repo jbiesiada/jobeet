550f2d8178bfd2a41a8a188cdda3bbe79683bc32
Yankers88 <jbies.jb@gmail.com>
2015-11-19 12:20:42 +0100

    affiliates

---
+++ src/Ens/JobeetBundle/Form/AffiliateType.php
@@ -0,0 +1,32 @@
+<?php
+namespace Ens\JobeetBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Ens\JobeetBundle\Entity\Affiliate;
+use Ens\JobeetBundle\Entity\Category;
+
+class AffiliateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('url')
+            ->add('email')
+            ->add('categories', null, array('expanded'=>true))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ens\JobeetBundle\Entity\Affiliate',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'affiliate';
+    }
+}
